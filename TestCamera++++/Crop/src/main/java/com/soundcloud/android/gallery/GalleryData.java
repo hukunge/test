cf77638f01444763acf983775930fbc0111df635
kellan <hukunge@163.com>
2017-12-06 10:27:26 +0800

    aaaa

--- TestCamera++++/Crop/src/main/java/com/soundcloud/android/gallery/GalleryData.java
+++ TestCamera++++/Crop/src/main/java/com/soundcloud/android/gallery/GalleryData.java
@@ -3,12 +3,13 @@ package com.soundcloud.android.gallery;
 import android.net.Uri;
 
 import java.io.File;
+import java.io.Serializable;
 
 /**
  * Created by kellan on 2017/12/4.
  */
 
-public class GalleryData {
+public class GalleryData implements Serializable{
     public File file;
     public Uri uri;
     public boolean isSelected;
--- TestCamera++++/Crop/src/main/java/com/soundcloud/android/gallery/GridAdapter.java
+++ TestCamera++++/Crop/src/main/java/com/soundcloud/android/gallery/GridAdapter.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.soundcloud.android.crop.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,9 +19,11 @@ import java.util.List;
 public class GridAdapter extends BaseAdapter {
     private final Activity ctx;
     private List<GalleryData> data;
+    private List<GalleryData> selected;
 
     public GridAdapter(Activity ctx) {
         this.ctx = ctx;
+        this.selected = new ArrayList<>();
     }
 
     public void setData(List<GalleryData> data) {
@@ -52,7 +55,15 @@ public class GridAdapter extends BaseAdapter {
             h = new ViewHolder();
 
             h.imageView = v.findViewById(R.id.image_view_image_select);
-            h.view = v.findViewById(R.id.view_alpha);
+            h.view = v.findViewById(R.id.select);
+
+            h.view.setOnClickListener(view -> {
+                GalleryData gd = data.get(position);
+                gd.isSelected = !gd.isSelected;
+                notifyDataSetChanged();
+
+                addOrRemove(gd);
+            });
 
             v.setTag(h);
         } else {
@@ -60,22 +71,34 @@ public class GridAdapter extends BaseAdapter {
         }
 
         GalleryData gd = data.get(position);
-//        if (gd.isSelected) {
-//            viewHolder.view.setAlpha(0.5f);
-//        } else {
-//            viewHolder.view.setAlpha(0.0f);
-//        }
+
+        if(gd.isSelected){
+            h.view.setImageResource(R.drawable.crop__ic_done);
+        }else {
+            h.view.setImageResource(R.drawable.crop__ic_cancel);
+        }
 
         Glide.with(ctx)
                 .load(gd.uri)
-//                .load(new File(gd.uri.getPath()))
                 .into(h.imageView);
 
         return v;
     }
 
+    private void addOrRemove(GalleryData d){
+        if(d.isSelected){
+            selected.add(d);
+        }else {
+            selected.remove(d);
+        }
+    }
+
+    public List<GalleryData> getSelected(){
+        return selected;
+    }
+
     private static class ViewHolder {
         public ImageView imageView;
-        public View view;
+        public ImageView view;
     }
 }
